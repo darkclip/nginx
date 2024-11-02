16508bb8bd6847c75d88387776deb839d5fecb69
darkclip <darkclip@users.noreply.github.com>
2024-11-03 01:09:38 +0800

    update

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -3,11 +3,10 @@
 sed -i "s#cd /tmp/openresty#\
 git clone https://github.com/arut/nginx-rtmp-module.git\\
 mv /tmp/nginx-rtmp-module /tmp/openresty/nginx-rtmp-module\\
-ls\\
 &#g" scripts/build-openresty
 
 sed -i "s#./configure \\\#&\\
---add-module=/tmp/openresty/nginx-rtmp-module \\\ \
+    --add-module=/tmp/openresty/nginx-rtmp-module \\\ \
 #g" scripts/build-openresty
 
 sed -i 's#make -j2#make -j$(getconf _NPROCESSORS_ONLN)#g' scripts/build-openresty
