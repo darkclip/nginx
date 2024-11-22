dacc1e7691955596054289b495e648351537e62b
darkclip <darkclip@users.noreply.github.com>
2024-11-22 18:38:35 +0800

    update

--- rootfs/usr/local/sbin/init.sh
+++ rootfs/usr/local/sbin/init.sh
@@ -2,7 +2,7 @@
 
 if [ ! "$(ls -A $DIR)" ]; then
     cp -r /data-install/* /data/
-    cp -r /data-preset/$exist /data/
+    cp -r /data-preset/* /data/
 fi
 service cron start
 nginx -g 'daemon off;'
