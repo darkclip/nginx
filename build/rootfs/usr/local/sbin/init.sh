892af3aeca3667c8ad4ceaf2be473ea525389855
darkclip <darkclip@users.noreply.github.com>
2024-11-23 16:25:03 +0800

    update

--- build/rootfs/usr/local/sbin/init.sh
+++ build/rootfs/usr/local/sbin/init.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-mkdir -p \
-/data/openresty/log \
-/data/openresty/cache \
-/data/acme/challenge \
-/data/www \
-/data/openresty/module \
-/data/openresty/http \
-/data/openresty/stream \
-/data/openresty/rtmp \
-/data/certs
+prepare_dirs=(
+    /data/openresty/log
+    /data/openresty/cache
+    /data/acme/challenge
+    /data/www
+    /data/openresty/module
+    /data/openresty/http
+    /data/openresty/stream
+    /data/openresty/rtmp
+    /data/certs
+)
+for check in ${prepare_dirs[@]}; do
+    if [ ! "$(ls -A $check &>/dev/null)" ]; then
+        mkdir -p $check
+    fi
+done
+
 dirs=$(ls -l /data-install | awk '/^d/ {print $NF}')
 data_dirs=($dirs)
 for exist in ${data_dirs[@]}; do
@@ -17,5 +24,6 @@ for exist in ${data_dirs[@]}; do
         cp -r /data-install/$exist /data/
     fi
 done
+
 service cron start
 nginx
